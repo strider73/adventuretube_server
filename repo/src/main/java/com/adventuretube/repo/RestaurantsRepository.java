fc0a76281d01052eff6a404776612248ae58ac53
strider73 <strider.lee@gmail.com>
2023-11-04 13:45:38 +1100

    fix the issue for 1) wrong parameter type 2) wrong member variable

--- repo/src/main/java/com/adventuretube/repo/RestaurantsRepository.java
+++ repo/src/main/java/com/adventuretube/repo/RestaurantsRepository.java
@@ -1,11 +1,11 @@
 package com.adventuretube.repo;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
 import com.adventuretube.model.Restaurants;
 import org.springframework.data.domain.Page;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
+import  org.springframework.data.domain.Pageable;
 
-public interface RestaurantsRepository extends JpaRepository <Restaurants,Long> {
+public interface RestaurantsRepository extends MongoRepository <Restaurants,Long> {
     Page<Restaurants> findAll(Pageable pageable);
 }
--- web/src/main/java/com/adventuretube/RestaurantsDataController.java
+++ web/src/main/java/com/adventuretube/RestaurantsDataController.java
@@ -17,15 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestaurantsDataController {
     private final RestaurantsDataService restaurantsDataService;
 
-    int page = 1; // Page number, 0-based index (first page is 0)
-    int size = 10; // Number of items per page
-    Sort sort = Sort.by(Sort.Order.asc("fieldName")); // Replace "fieldName" with the field you want to sort by
-    Pageable pageable = PageRequest.of(page, size, sort);
+
 
 
 
     @GetMapping
     public void getRestaurants(){
+
+        int page = 1; // Page number, 0-based index (first page is 0)
+        int size = 10; // Number of items per page
         Sort sort = Sort.by(Sort.Order.asc("name"));
         Pageable pageable = PageRequest.of(page,size);
         restaurantsDataService.getRestaurants(pageable);
