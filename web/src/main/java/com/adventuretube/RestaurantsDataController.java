59a4c459afc2c363383174ac57ea463559b2b858
strider73 <strider.lee@gmail.com>
2023-12-04 23:33:59 +1100

    RestaurantsDataController
     getRestaurants
     getRestaurants
    
     will return restaurants  json

--- web/src/main/java/com/adventuretube/RestaurantsDataController.java
+++ web/src/main/java/com/adventuretube/RestaurantsDataController.java
@@ -12,6 +12,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.PageRequest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,25 +32,29 @@ public class RestaurantsDataController {
 
 
     @GetMapping
-    public void getRestaurants(){
+    public ResponseEntity<List<Restaurants>>  getRestaurants(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size
+            ){
 
-        int page = 1; // Page number, 0-based index (first page is 0)
-        int size = 10; // Number of items per page
+//        int page = 1; // Page number, 0-based index (first page is 0)
+//        int size = 10; // Number of items per page
         Sort sort = Sort.by(Sort.Order.asc("name"));
         Pageable pageable = PageRequest.of(page,size);
         Page<Restaurants> restaurantPage =   restaurantsDataService.getRestaurants(pageable);
-        for (Restaurants restaurant : restaurantPage.getContent()) {
-            System.out.println("==============BellaMun==========");
-            System.out.println(restaurant.getName());
-            System.out.println(restaurant.getLocation());
-            System.out.println(restaurant.getLocation().getCoordinates());
-        }
+        return new ResponseEntity<>(restaurantPage.getContent(), HttpStatus.OK);
+//        for (Restaurants restaurant : restaurantPage.getContent()) {
+//            System.out.println("==============BellaMun==========");
+//            System.out.println(restaurant.getName());
+//            System.out.println(restaurant.getLocation());
+//            System.out.println(restaurant.getLocation().getCoordinates());
+//        }
     }
 
 
 
     @GetMapping("/near")
-    public  void getRestaurntsByNear(
+    public  ResponseEntity<List<Restaurants>> getRestaurntsByNear(
             @RequestParam("latitude") double latitude,
             @RequestParam("longitude") double longitude,
             @RequestParam("maxDistance")  double maxDistance
@@ -60,13 +66,13 @@ public class RestaurantsDataController {
         Point current = new Point(new Position(longitude, latitude));
 //      Point current = new Point(new Position(-73.93414657, 40.82302903));
         List<Restaurants> nearbyRestaurants = restaurantsDataService.findRestaurantsNearLocation(current, maxDistance * meterPerMile);
-
-        for (Restaurants restaurants : nearbyRestaurants) {
-            // Print restaurant information (you can customize the printing as needed)
-            System.out.println("Restaurant Name: " + restaurants.getName());
-            System.out.println("Restaurant Address: " + restaurants.toString());
-            // Add more details as necessary
-        }
+        return new ResponseEntity<>(nearbyRestaurants, HttpStatus.OK);
+//        for (Restaurants restaurants : nearbyRestaurants) {
+//            // Print restaurant information (you can customize the printing as needed)
+//            System.out.println("Restaurant Name: " + restaurants.getName());
+//            System.out.println("Restaurant Address: " + restaurants.toString());
+//            // Add more details as necessary
+//        }
     }
 
 }
